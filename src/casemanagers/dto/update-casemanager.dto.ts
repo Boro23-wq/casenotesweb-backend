05741683983a618b405004395c41eea54dbf52cc
Sintu Boro <sintuboro@Sintus-MBP.lan>
2022-11-06 13:43:12 -0600

    added not found exceptions

--- src/casemanagers/dto/update-casemanager.dto.ts
+++ src/casemanagers/dto/update-casemanager.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCasemanagerDto } from './create-casemanager.dto';
 
 export class UpdateCasemanagerDto extends PartialType(CreateCasemanagerDto) {}
--- src/cases/dto/update-case.dto.ts
+++ src/cases/dto/update-case.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCaseDto } from './create-case.dto';
 
 export class UpdateCaseDto extends PartialType(CreateCaseDto) {}
